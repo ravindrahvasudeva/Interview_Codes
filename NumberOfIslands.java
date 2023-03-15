977cc135f4077d2806f0a197d854be150b4f35f6
ravindrahv <ravindrahvasudeva@gmail.com>
2023-03-15 19:21:16 +0530

    number of island

--- NumberOfIslands.java
+++ NumberOfIslands.java
@@ -25,3 +25,29 @@ class Solution {
      calldfs(grid, i, j+1);//right
  }
  }
+
+ /*
+  #Number of Islands
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+
+
+Example 2:
+
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3
+  */
