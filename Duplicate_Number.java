7bb3d1026e6dd7d76984ce85c1f376e1b6a36ca9
ravindrahv <ravindrahvasudeva@gmail.com>
2023-03-19 22:37:14 +0530

    Duplicate_Number

--- Duplicate_Number.java
+++ Duplicate_Number.java
@@ -1,3 +1,32 @@
 public class Duplicate_Number {
+        public int findDuplicate(int[] nums) {
+            Arrays.sort(nums);
+            Set<Integer> caught = new HashSet<Integer>();
+            for(int n : nums){
+                if(caught.contains(n)){//contains gives true or false
+                    return n;
+                }
+                 caught.add(n);//if not in hashset add item
+            }
+            return -1;
+        }}
+//---------------------------------------------------------------------------    
+        /*
+        Find the Duplicate Number
+        Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
     
-}
+    There is only one repeated number in nums, return this repeated number.
+    
+    You must solve the problem without modifying the array nums and uses only constant extra space.
+    
+     
+    
+    Example 1:
+    
+    Input: nums = [1,3,4,2,2]
+    Output: 2
+    Example 2:
+    
+    Input: nums = [3,1,3,4,2]
+    Output: 3
+    */
