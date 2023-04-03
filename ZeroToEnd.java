66d07fa174d820405096ba4897fce3d068d135f0
ravindrahv <ravindrahvasudeva@gmail.com>
2023-04-03 23:37:38 +0530

    move all 0 to end

--- ZeroToEnd.java
+++ ZeroToEnd.java
@@ -26,3 +26,8 @@ public class ZeroToEnd {
 		}
 	}
 }
+/*
+ Given an array of random numbers, Push all the zero’s of a given array to the end of the array. 
+ For example, if the given arrays is {1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0}, it should be changed to {1, 9, 8, 4, 2, 7, 6, 0, 0, 0, 0}. 
+
+ */
